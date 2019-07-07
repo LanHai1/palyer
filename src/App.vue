1b79f29804faac01bb8b4ed2969d76ddfc6e5455
lanhai <1444133004@qq.com>
2019-07-07 19:44:01 +0800

    搜索结果组件路由完成

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
-  
+  <div id="player">
+    <h2 class="title">某云音乐</h2>
+    <div class="search">
+      <input type="text"/>
+      <button >
+        <span class="iconfont icon-search"></span>
+      </button>
+    </div>
+    <div class="tab-wrapper">
+      <!-- tab栏 -->
+      <div class="tab-bar">
+        <router-link active-class="active" to="/musicSearch" tag="span" class="bar-item">搜索结果</router-link>
+        <a class="bar-item">歌曲播放</a>
+        <a class="bar-item">mv</a>
+        <a class="bar-item">歌曲评论</a>
+      </div>
+      <!-- 对应的内容区域 -->
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+@import url("./assets/css/index.css");
+@import url("./assets/css/iconfont.css");
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+// 导入路由
+import VueRouter from "vue-router";
+
+// 模块化机制编程
+Vue.use(VueRouter);
+
+// 组件
+import musicSearch from "./components/musicSearch.vue";
+
+//  定义路由
+const routes = [
+  // 搜索结果
+  {
+    path: "/musicSearch",
+    component: musicSearch
+  }
+];
+
+// 创建路由
+const router = new VueRouter({
+  routes
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  // 挂载路由
+  router,
+  render: h => h(App)
+}).$mount("#app");
